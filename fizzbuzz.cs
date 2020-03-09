5ecc79c1e1304797d0bb59e83e0d98ab01ede74f
Ellie <elliemmanifold@gmail.com>
2020-03-09 14:06:02 +0000

    feat: handle multiples of 3, 5 and both

--- fizzbuzz.cs
+++ fizzbuzz.cs
@@ -1,12 +1,23 @@
 namespace FizzBuzz
 {
 
-  public class FizzBuzz 
+  public class FizzBuzz
   {
 
     public static string GetOutput(int number)
     {
-
+      if ((number % 3 == 0) && (number % 5 == 0))
+      {
+        return "fizzbuzz"
+      }
+      else if (number % 3 == 0)
+      {
+        return "fizz"
+      }
+      else if (number % 5 == 0)
+      {
+        return "buzz"
+      }
     }
   }
 }
